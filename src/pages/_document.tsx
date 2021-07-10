fce61b15dddeb1e0249786e7b95fdbbe7dc558e6
Gabriel da Silva Nascimento <gabriel.nascimento@compasso.com.br>
2021-07-10 14:49:29 -0300

    Create a Favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700;900&display=swap" rel="stylesheet" />
-
+          <link rel="icon" type="image/png" sizes="16x16" href="/logo-ico.png"/>
         </Head>
         <body>
           <Main />
--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -1,46 +1,53 @@
+import Head from 'next/head';
 import { TabPanel, Tab } from '../components';
 
 import styles from '../styles/services.module.scss';
 
 export default function Services() {
   return (
-    <main className={styles.main}>
-      <section className={styles.sectionServices}>
-        <h1>Nossos serviços</h1>
+    <>
+      <Head>
+        <title>Serviços | AmetaServico</title>
+      </Head>
 
-        <TabPanel className={styles.contentPanel}>
-          <Tab title="Site Survey">
-            <h2>Neste serviço é incluso: </h2> <br />
+      <main className={styles.main}>
+        <section className={styles.sectionServices}>
+          <h1>Nossos serviços</h1>
 
-            <p>
-              - Engenharia <br />
-              - Infraestrutura <br />
-              - RF <br />
-              - TX
-            </p>
-          </Tab>
-          <Tab title="Documentações">
-            <h2>Neste serviço é incluso: </h2> <br />
+          <TabPanel className={styles.contentPanel}>
+            <Tab title="Site Survey">
+              <h2>Neste serviço é incluso: </h2> <br />
 
-            <p>
-              - Projeto Provisório de Instalação (PPI) <br />
-              - Projeto Definitivo de Instalação (PDI) <br />
-              - Elaboração de War Room (Site Solution) <br />
-              - Solicitação de Compartilhamento <br />
-              - Laudo Radiométrico 
-            </p>
-          </Tab>
-          <Tab title="Consultoria">
-            <h2>Neste serviço é incluso: </h2> <br />
+              <p>
+                - Engenharia <br />
+                - Infraestrutura <br />
+                - RF <br />
+                - TX
+              </p>
+            </Tab>
+            <Tab title="Documentações">
+              <h2>Neste serviço é incluso: </h2> <br />
 
-            <p>
-              - Site Survey <br />
-              - Documentação <br />
-              - Elaboração de War Room (Site Solution)) <br />
-            </p>
-          </Tab>
-        </TabPanel>
-      </section>
-    </main>
+              <p>
+                - Projeto Provisório de Instalação (PPI) <br />
+                - Projeto Definitivo de Instalação (PDI) <br />
+                - Elaboração de War Room (Site Solution) <br />
+                - Solicitação de Compartilhamento <br />
+                - Laudo Radiométrico 
+              </p>
+            </Tab>
+            <Tab title="Consultoria">
+              <h2>Neste serviço é incluso: </h2> <br />
+
+              <p>
+                - Site Survey <br />
+                - Documentação <br />
+                - Elaboração de War Room (Site Solution)) <br />
+              </p>
+            </Tab>
+          </TabPanel>
+        </section>
+      </main>
+    </>
   );
 }
