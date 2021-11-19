1f9d0172b92f62961785102fb0000786dca88e7e
Gabriel da Silva Nascimento <gabriel.nascimento@compasso.com.br>
2021-11-19 16:18:26 -0300

    add metatags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,10 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700;900&display=swap" rel="stylesheet" />
           <link rel="icon" type="image/png" sizes="16x16" href="/logo-ico.png"/>
+
+          <meta property="og:title"              content="Ameta Serviços" />
+          <meta property="og:description"        content="Está procurando serviços de qualidade em telecomunicação?" />
+          <meta property="og:image"              content="images/logo.png" />
         </Head>
         <body>
           <Main />
