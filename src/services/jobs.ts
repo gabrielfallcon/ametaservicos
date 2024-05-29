028713c53bcc9650b9453a45cce91ae773b43696
Gabriel da Silva Nascimento <974511@lojasrenner.com.br>
2024-05-29 00:27:58 -0300

    fix: ajustes de textos

--- src/services/jobs.ts
+++ src/services/jobs.ts
@@ -12,10 +12,10 @@ export const dataJobs = [
         id: 1,
         key: 'tecnologia',
         title: 'Técnico vistoriador de Telecomunicações',
-        description: 'Conhecimento nos equipamentos Ericsson.',
+        description: ' Conhecimento em equipamentos de telecomunicações.',
         requirements: 'Para esta vaga é desejável que o candidato tenha experiência em campo. Daremos treinamento se necessário.',
         local: 'Sao paulo - SP',
-        typeHiring: 'PJ'
+        typeHiring: 'CLT'
     },
     
 ]
